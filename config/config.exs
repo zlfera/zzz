7273f218c41e302ab1e463bd748ab9af8758a573
zlf <zlfera@gmail.com>
2024-07-28 17:11:34 +0800

    up

--- config/config.exs
+++ config/config.exs
@@ -43,16 +43,16 @@ config :esbuild,
   ]
 
 # Configure tailwind (the version is required)
-# config :tailwind,
-# version: "3.3.2",
-# default: [
-# args: ~w(
-#  --config=tailwind.config.js
-# --input=css/app.css
-# --output=../priv/static/assets/app.css
-# ),
-# cd: Path.expand("../assets", __DIR__)
-# ]
+ config :tailwind,
+ version: "3.3.2",
+ default: [
+ args: ~w(
+  --config=tailwind.config.js
+ --input=css/app.css
+ --output=../priv/static/assets/app.css
+ ),
+ cd: Path.expand("../assets", __DIR__)
+ ]
 
 # Configures Elixir's Logger
 config :logger, :console,
