323a57ee11263521528f96e748ae8566bf3ec430
zlf <zlfera@gmail.com>
2024-07-28 16:48:37 +0800

    up

--- mix.exs
+++ mix.exs
@@ -70,8 +70,8 @@ defmodule Zlfera.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind default", "esbuild default"]
-      # "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.build": ["tailwind default", "esbuild default"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
