d9583febb8f1f1e357434e0e9fbc81d5037d336d
zlf <zlfera@gmail.com>
2024-07-28 15:26:43 +0800

    up

--- lib/zlfera_web/components/layouts.ex
+++ lib/zlfera_web/components/layouts.ex
@@ -1,5 +1,14 @@
 defmodule ZlferaWeb.Layouts do
   use ZlferaWeb, :html
 
+  def get_text do
+    if File.exists?("./priv/static/index.html") do
+      url = "./priv/static/index.html" |> File.read!()
+      Jason.decode!(url)
+    else
+      %{"content" => "", "text_authors" => ""}
+    end
+  end
+
   embed_templates "layouts/*"
 end
