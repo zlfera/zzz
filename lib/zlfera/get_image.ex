6964e81c742055a0e3cdb80a677b24c67925d1df
zlf <zlfera@gmail.com>
2024-07-30 07:12:49 +0800

    up

--- lib/zlfera/get_image.ex
+++ lib/zlfera/get_image.ex
@@ -36,7 +36,7 @@ defmodule Zlfera.GetImage do
     [_, i] = Regex.split(~r{\(}, get_img())
     [i, _] = Regex.split(~r{\)}, i)
     image = HTTPoison.get!(i, recv_timeout: 10000).body
-    File.write("./priv/static/background_image.jpeg", image)
+    File.write("./priv/static/images/background_image.jpeg", image)
     # html = HTTPoison.get!("www.youmile.vip/", recv_timeout: 10000).body
     # File.write("./priv/static/index.html", get_text())
   end
